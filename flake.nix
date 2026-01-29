f57991b9e161642c6f5872802788c14b8e1903ed
Alex Bethel <alex.b.bethel@gmail.com>
2026-01-28 21:02:20 -0700

    Flake set up for Tatum website

--- flake.nix
+++ flake.nix
@@ -6,12 +6,18 @@
             url = "github:nix-community/nixos-generators";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+
+        tatum-website = {
+            url = "git+ssh://root@plixers/root/tatum?ref=main";
+            flake = false;
+        };
     };
-    outputs = {nixpkgs, nixos-generators, ...}: rec {
+    outputs = {nixpkgs, nixos-generators, tatum-website, ...}: rec {
         nixosConfigurations.TriBlankleyBlog = nixpkgs.lib.nixosSystem {
             system = "x86_64-linux";
             modules = [
                 ./configuration.nix
+                "${tatum-website}/module.nix"
                 nixos-generators.nixosModules.all-formats
             ];
         };
