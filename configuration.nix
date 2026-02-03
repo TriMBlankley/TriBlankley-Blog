0c66f3b131ae9a2454a9c7ad5c40cee11d69428e
TriMBlankley <triblankley@gmail.com>
2026-02-02 20:09:10 -0700

    Changed the logo, about the artist blurb, the text color, and lots of layout changes to the item page.

--- configuration.nix
+++ configuration.nix
@@ -2,6 +2,7 @@
 {
     users.users.root.openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL4c5ImNWJqP9UDm8FZDOzbRzgQ/pTP7LXuXk+w9UQRm shitasshp@nixos"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICVk66vVAoJdPMphbh9Ex0ue6QKQhD9pX18EF6bgkLob alex@fuzzpaws"
     ];
 
     nixpkgs.config.allowUnfree = true;
